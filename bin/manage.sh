7679949e96402273f0698df9349eab3b08c4f2f2
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2017-08-20 20:18:17 +0100

    added health and template

--- bin/manage.sh
+++ bin/manage.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
+
 if [[ -z ${CONSUL} ]]; then
 	echo "Missing CONSUL Environment Variable" 
 	exit 1
 fi 
 
 preStart() {
-	consul-template \
+  export HOST=$(hostname) 
+  consul-template \
 		-once \
 		-consul-addr ${CONSUL}:8500 \
 		-template "/etc/cassandra.yaml.ctmpl:/etc/cassandra/cassandra.yaml"
 }
 
-
-
-
-
-
-
-
-
-
-
+health() {
+    local privateIp=$(ip addr show eth0 | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}')
+    /usr/bin/curl --fail -s -o /dev/null http://${privateIp}:7000
+}
+$1 
